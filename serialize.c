dd1475e2d159ad7b18ee974b4a3120451009d45c
David Barach <dbarach@bxb-lds-025>
2009-02-02 08:00:42 -0500

    1) fix typo in clib_net_to_host_unaligned_mem_xxx 2) [edressel] use network byte order in serialize fcns 3) make va_serialize() global instead of static.

--- serialize.c
+++ serialize.c
@@ -280,7 +280,7 @@ void unserialize_check_magic (serialize_main_t * m, void * magic,
     goto bad;
 }
 
-static clib_error_t *
+clib_error_t *
 va_serialize (serialize_main_t * m, va_list * va)
 {
   serialize_function_t * f = va_arg (*va, serialize_function_t *);
@@ -364,6 +364,7 @@ void serialize_fill_buffer (serialize_main_t * m, u32 n_must_read)
     }
 
   vec_add2 (m->buffer, b, n);
+
   error = m->read (m, b, &n_read);
   if (error)
     {
