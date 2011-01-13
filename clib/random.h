d15a83165537645f6fc83d8804254de8a535d1f5
David Barach <dbarach@sw-eclipse-14.cisco.com>
2011-01-13 15:00:18 -0500

    Doxygen tags

--- clib/random.h
+++ clib/random.h
@@ -28,8 +28,29 @@
 #include <clib/vec.h>		/* for vec_resize */
 #include <clib/format.h>	/* for unformat_input_t */
 
-/* This specific random number generator is described in
-   "Numerical Recipes in C", 2nd edition, page 284. */
+/** \file
+    Linear Congruential Random Number Generator
+
+    This specific random number generator is described in
+    "Numerical Recipes in C", 2nd edition, page 284. If you need
+    random numbers with really excellent statistics, take a look
+    at Chapter 7...    
+    
+    By definition, a linear congruential random number generator
+    is of the form: rand[i+1] = a*rand[i] + c (mod m) for specific
+    values of (a,c,m). 
+    
+    In this case, choose m = 2**32 and use the low-order 32-bits of
+    the 64-bit product a*N[i]. Knuth suggests the use of a=1664525,
+    H.W. Lewis has tested C=1013904223 extensively. This routine is
+    reputedly as good as any 32-bit LCRN, and costs only a single
+    multiply-add.
+
+    Several variants: 32/64-bit, machine word width,
+    f64 on the closed interval [0,1].
+*/
+
+/** \brief 32-bit random number generator */
 always_inline u32 random_u32 (u32 * seed)
 {
   *seed = (1664525 * *seed) + 1013904223;
@@ -38,6 +59,7 @@ always_inline u32 random_u32 (u32 * seed)
 /* External test routine. */
 int test_random_main (unformat_input_t * input);
 
+/** \brief Maximum value returned by random_u32() */
 always_inline u32 random_u32_max (void)
 { return 0xffffffff; }
 
@@ -45,6 +67,7 @@ always_inline u32 random_u32_max (void)
 
 #include <unistd.h>		/* for getpid */
 
+/** \brief Default random seed (unix/linux user-mode) */
 always_inline uword random_default_seed (void)
 { return getpid (); }
 
@@ -54,6 +77,7 @@ always_inline uword random_default_seed (void)
 
 #include <linux/sched.h>	/* for jiffies */
 
+/** \brief Default random seed (Linux kernel) */
 always_inline uword random_default_seed (void)
 { return jiffies; }
 
@@ -66,6 +90,10 @@ always_inline u32 random_default_seed (void)
 { return standalone_random_default_seed; }
 #endif
 
+/** \brief 64-bit random number generator
+
+    created via two calls to random_u32(). Quick and dirty.
+*/
 always_inline u64
 random_u64 (u32 * seed)
 {
@@ -76,6 +104,8 @@ random_u64 (u32 * seed)
   return result;
 }
 
+/** \brief machine word size random number generator */
+
 always_inline uword
 random_uword (u32 * seed)
 {
@@ -85,10 +115,16 @@ random_uword (u32 * seed)
     return random_u32 (seed);
 }
 
-/* Return random float between 0 and 1. */
+/** \brief Generate f64 random number in the interval [0,1] */
 always_inline f64 random_f64 (u32 * seed)
 { return (f64) random_u32 (seed) / (f64) random_u32_max (); }
 
+/** \brief Generate random character vector
+
+    From the alphabet a-z, lower case.
+    Returns a vector of the supplied length which is NOT guaranteed to be
+    NULL-terminated. FIXME?
+*/
 always_inline u8 *
 random_string (u32 * seed, uword len)
 {
