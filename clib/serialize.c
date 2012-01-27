f379a354d5aee16145b016fc309b22bc312cf6d7
David Barach <dbarach@sw-eclipse-14.cisco.com>
2012-01-27 10:18:35 -0500

    Unserialize_check_magic() must match serialize_cstring

--- clib/serialize.c
+++ clib/serialize.c
@@ -564,7 +564,7 @@ void unserialize_check_magic (serialize_main_t * m, void * magic,
   u32 l;
   void * d;
 
-  unserialize_integer (m, &l, sizeof (l));
+  l = unserialize_likely_small_unsigned_integer (m);
   if (l != magic_bytes)
     {
     bad:
